afb94dee9287e756d45392fcc0885befd9647331
Melody <Melody@Melodys-MacBook.local>
2017-12-31 10:43:15 -0700

    setting up server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const massive = require('massive');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+massive(process.env.CONNECTION_STRING).then(db => {
+    app.set('db',db)
+})
+
+const port = process.env.PORT
+app.listen(port, () => console.log(`listening on port ${port}`));
