eda58717efaa6d623a421522069fac53ce53c24e
Melody <Melody@Melodys-MacBook.local>
2018-01-15 14:30:52 -0700

    added sesson

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const massive = require('massive');
+const session = require('express-session');
 const controller = require('./controller');
 require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(session({
+    secret: process.env.SESSION_SECRET, 
+    saveUninitialized: false, 
+    resave: false
+}));
 
 massive(process.env.CONNECTION_STRING).then(db => {
     app.set('db', db)
+}).catch(error => {
+    console.log('error', error)
 });
 
 app.post('/api/recipes', controller.post)
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Auth0Lock from 'auth0-lock';
 
-const Login = () => {
-    return (
-        <div>
-            <div>Site Name/Logo</div>
-            <div><Link path to='/home'>Login/Register</Link></div>
-        </div>
-    )
+class Login extends Component {
+    render() {
+
+        return (
+            <div>
+                <div>Site Name/Logo</div>
+                <div><Link path to='/home'>Login/Register</Link></div>
+            </div>
+        )
+    }
 }
 
 export default Login;
